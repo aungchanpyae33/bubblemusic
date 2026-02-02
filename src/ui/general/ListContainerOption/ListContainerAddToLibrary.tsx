f4382c21111f2f2d1efba63d843d8939404894b9
aungchanpyae33 <khantlu24@gmail.com>
2026-02-02 12:21:16 +0630

    add: nextjs-top-loader to delet action

--- src/ui/general/ListContainerOption/ListContainerAddToLibrary.tsx
+++ src/ui/general/ListContainerOption/ListContainerAddToLibrary.tsx
@@ -10,7 +10,8 @@ import {
   SongListContext,
   SongListValue,
 } from "@/ui/playlist/playlistOption/ContextSongListContainer";
-import { useRouter } from "next/navigation";
+import { useRouter } from "nextjs-toploader/app";
+import { useTopLoader } from "nextjs-toploader";
 
 interface ListContainerAddToLibraryProps {
   id: string;
@@ -38,23 +39,25 @@ function ListContainerAddToLibrary() {
   const { id, type, source } = useContext(SongListContext) as SongListValue;
 
   const router = useRouter();
-
+  const loader = useTopLoader();
   const queryClient = useQueryClient();
   const [itemSource, setItemSource] = useState(isAdd(source));
   useEffect(() => {
     setItemSource(isAdd(source));
   }, [source]);
   async function ActionToLibraryFn() {
+    loader.start();
     setItemSource(!itemSource);
     const { data, error } = await modifyLib({ id, type, source });
     if (error) {
-      console.log(error);
+      loader.done();
     } else {
       if (data) {
         queryClient.setQueryData(["user-library"], {
           data,
           error: null,
         });
+        loader.done();
       }
       if (source === "create") {
         router.push("/");
