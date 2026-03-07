ee8a5a9d4214362593426be069661c3f07ca9c1c
aungchanpyae33 <khantlu24@gmail.com>
2026-03-08 01:21:37 +0630

    refactor: use seperate componet for login button

--- src/ui/user/UserInfo.tsx
+++ src/ui/user/UserInfo.tsx
@@ -1,6 +1,6 @@
 import UserProfile from "./UserProfile";
 import { createClient } from "@/database/server";
-import NoThankYouPreFetchLink from "../general/NoThankYouPreFetchLink";
+import UserLoginButton from "./UserLoginButton";
 
 async function UserInfo() {
   const supabase = await createClient();
@@ -10,15 +10,7 @@ async function UserInfo() {
 
   const user = data?.claims;
 
-  return user ? (
-    <UserProfile user={user} />
-  ) : (
-    <NoThankYouPreFetchLink href={"/auth/login"}>
-      <button className=" bg-surface-1  hover:scale-105   active:scale-90 h-[40px] px-2  border-opacity-15 border border-borderFull rounded-sm flex items-center ">
-        ဝင်ရန်
-      </button>
-    </NoThankYouPreFetchLink>
-  );
+  return user ? <UserProfile user={user} /> : <UserLoginButton />;
 }
 
 export default UserInfo;
