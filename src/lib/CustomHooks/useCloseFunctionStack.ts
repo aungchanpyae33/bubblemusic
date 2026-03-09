188cd65127264b2639b7e587c3b50d6ec8b29285
aungchanpyae33 <khantlu24@gmail.com>
2026-03-09 14:22:49 +0630

    fix: replace with closeFunctionStack
    replace useCloseFunctionForFull with stack because the only reason i use full is because of previous full case (with classlist z-50) case. Now as it is using with portal , it is no longer need. This new Function should be default to anything that not to toggleContent but with open-something case

---
+++ src/lib/CustomHooks/useCloseFunctionStack.ts
@@ -0,0 +1,44 @@
+import React, { RefObject, useEffect } from "react";
+import { useMoreOptionStackContext } from "@/ui/trackComponent/MoreOptionStackContext";
+
+// this function do close the portal when escape is pressed , it also manage the stack for inner child components
+function useCloseFunctoionStack(
+  value: boolean,
+  fun:
+    | React.Dispatch<React.SetStateAction<boolean>>
+    | ((value: boolean) => void),
+  closeElement?: RefObject<HTMLButtonElement | null>,
+) {
+  const { stack, setStack } = useMoreOptionStackContext();
+
+  // stack are 0 === parent , 1 === child , 2 === grand child etc..
+  useEffect(() => {
+    function closeSearch(e: KeyboardEvent) {
+      if (e.key === "Escape" && value === true) {
+        e.preventDefault();
+
+        // stack === 0 means it is the parent component
+        // i use open (boolean) only  for parent , inner child state are paired with stack number
+        if (stack === 0) {
+          fun(false);
+          if (!closeElement) return;
+          closeElement.current!.focus();
+          return;
+        }
+
+        // decrease the stack count because of clicking triiger
+        const newStack = Math.max(0, stack - 1);
+        setStack(newStack);
+      }
+    }
+    if (value) {
+      document.body.addEventListener("keydown", closeSearch);
+    }
+
+    return () => {
+      document.body.removeEventListener("keydown", closeSearch);
+    };
+  }, [value, fun, closeElement, stack, setStack]);
+}
+
+export default useCloseFunctoionStack;
