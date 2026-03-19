c2129df8fa12f481b5ca1cda4572b7fc79c2b928
aungchanpyae33 <khantlu24@gmail.com>
2026-03-20 01:34:30 +0630

    refactor: refactor the component to accpect props that direct linked style to IconWrapper

--- src/ui/general/icon/VerticalThreeDots.tsx
+++ src/ui/general/icon/VerticalThreeDots.tsx
@@ -1,8 +1,22 @@
 import { EllipsisVertical } from "lucide-react";
 import IconWrapper from "../IconWrapper";
-
-function VerticalThreeDots() {
-  return <IconWrapper Icon={EllipsisVertical} size="small" />;
+interface VerticalThreeDotsProps extends React.ComponentProps<"svg"> {
+  size?: "exLarge" | "large" | "medium" | "small";
+  notClickable?: boolean;
+}
+function VerticalThreeDots({
+  size,
+  notClickable,
+  ...props
+}: VerticalThreeDotsProps) {
+  return (
+    <IconWrapper
+      Icon={EllipsisVertical}
+      size={size}
+      notClickable={notClickable}
+      {...props}
+    />
+  );
 }
 
 export default VerticalThreeDots;
