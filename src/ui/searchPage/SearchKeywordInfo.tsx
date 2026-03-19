4f2f55736f2d198493c1dca4d0b0c3427af006eb
aungchanpyae33 <khantlu24@gmail.com>
2026-03-20 01:30:40 +0630

    ui: add icon beside text for Search Page

--- src/ui/searchPage/SearchKeywordInfo.tsx
+++ src/ui/searchPage/SearchKeywordInfo.tsx
@@ -1,10 +1,15 @@
 import { getTranslations } from "next-intl/server";
+import IconWrapper from "../general/IconWrapper";
+import { SearchCheck } from "lucide-react";
 
 async function SearchKeywordInfo({ query }: { query: string }) {
   const b = await getTranslations("block");
   return (
-    <div className="p-2">
-      {b("searchPage.searchResultFor", { searchKeyword: `"${query}"` })}
+    <div className="p-2 text-ink-400 flex gap-2">
+      <IconWrapper Icon={SearchCheck} className=" shrink-0 " />
+      <div className=" truncate">
+        {b("searchPage.searchResultFor", { searchKeyword: `"${query}"` })}
+      </div>
     </div>
   );
 }
