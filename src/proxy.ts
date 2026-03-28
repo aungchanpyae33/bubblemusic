80444ffa5d044989d01dcfcec809c3bf7023923d
aungchanpyae33 <khantlu24@gmail.com>
2026-03-28 13:19:04 +0630

    feat:change to  only restrict page when accessing the auth page when user is authenticated

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,3 +1,4 @@
+// proxy is copied from supabase doc example
 import { createServerClient } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
 
@@ -39,15 +40,10 @@ async function updateSession(request: NextRequest) {
   // with the Supabase client, your users may be randomly logged out.
   const { data } = await supabase.auth.getClaims();
   const user = data?.claims;
-  if (
-    request.nextUrl.pathname !== "/" &&
-    !user &&
-    !request.nextUrl.pathname.startsWith("/login") &&
-    !request.nextUrl.pathname.startsWith("/auth")
-  ) {
-    // no user, potentially respond by redirecting the user to the login page
+  // 2️ If user IS logged in and tries to access /auth pages, redirect to /root
+  if (user && request.nextUrl.pathname.startsWith("/auth")) {
     const url = request.nextUrl.clone();
-    url.pathname = "/auth/login";
+    url.pathname = "/"; // back to   default root page
     return NextResponse.redirect(url);
   }
 
