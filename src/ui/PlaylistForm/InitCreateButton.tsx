4bc00067f844d13c8f8d152e8767ddca51c130f0
aungchanpyae33 <khantlu24@gmail.com>
2026-03-28 12:38:34 +0630

    fix: conditional render relative to userInfo. (f567bfb)

--- src/ui/PlaylistForm/InitCreateButton.tsx
+++ src/ui/PlaylistForm/InitCreateButton.tsx
@@ -1,19 +1,23 @@
+import { useUserInfoContext } from "@/Context/ContextUserInfo";
 import {
   createToPlaylistModalBoxAction,
   useCreateToPlaylist,
 } from "@/lib/zustand";
 import IconWrapper from "@/ui/general/IconWrapper";
 import { Plus } from "lucide-react";
-import { useRef } from "react";
 
 function InitCreateButton() {
-  const buttonRef = useRef<HTMLButtonElement | null>(null);
+  const { userInfo } = useUserInfoContext();
   const createToPlaylistModalBoxAction = useCreateToPlaylist(
     (state: createToPlaylistModalBoxAction) =>
       state.createToPlaylistModalBoxAction,
   );
+  if (!userInfo) return null;
   return (
-    <button className="mr-2" onClick={() => createToPlaylistModalBoxAction({})}>
+    <button
+      className=" absolute h-full flex items-center right-2"
+      onClick={() => createToPlaylistModalBoxAction({})}
+    >
       <IconWrapper size="large" Icon={Plus} />
     </button>
   );
