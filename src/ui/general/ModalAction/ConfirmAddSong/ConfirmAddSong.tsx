cd4975c04df37e6b2bb5dfc0075e46a50c2dd815
aungchanpyae33 <khantlu24@gmail.com>
2026-04-04 23:38:25 +0630

    fix: add warning text

--- src/ui/general/ModalAction/ConfirmAddSong/ConfirmAddSong.tsx
+++ src/ui/general/ModalAction/ConfirmAddSong/ConfirmAddSong.tsx
@@ -1,4 +1,6 @@
 "use client";
+import Button from "@/components/button/Button";
+import { closeModalBox } from "@/lib/closeModalBox";
 import useAddSongMutate from "@/lib/CustomHooks/mutation/AddSongMutate";
 import {
   isSongExistModalBox,
@@ -7,9 +9,11 @@ import {
   useIsExistSongsModalBox,
 } from "@/lib/zustand";
 import { useTranslations } from "next-intl";
+import { toast } from "sonner";
 
 function ConfirmAddSong() {
   const w = useTranslations("Warning");
+  const toa = useTranslations("Toast");
   const { playlistId, songId, originParentTriggerRef, cover_url } =
     useIsExistSongsModalBox(
       (state: isSongExistModalBox) => state.isSongExistModalBox,
@@ -18,31 +22,29 @@ function ConfirmAddSong() {
   const setIsSongExist = useIsExistSongsModalBox(
     (state: songExistActionModalBox) => state.setIsSongExistModalBox,
   );
-  const mutation = useAddSongMutate(
-    playlistId,
-    cover_url,
-    originParentTriggerRef,
-  );
+  const mutation = useAddSongMutate(playlistId, cover_url);
   function handleAdd() {
+    closeModalBox(setIsSongExist, originParentTriggerRef);
+    const toastId = toast.loading(toa("loading")); //
     mutation.mutate({
       playlistId,
       songId,
+      toastId,
     });
   }
   return (
-    <div className=" w-full flex justify-end gap-3">
-      <button
-        className="bg-surface-1 p-2 rounded-lg"
-        onClick={() => {
-          setIsSongExist(undefined);
-        }}
-      >
-        {" "}
-        {w("cancel")}
-      </button>
-      <button className="bg-surface-1 p-2 rounded-lg" onClick={handleAdd}>
-        {w("add")}
-      </button>
+    <div className=" w-full flex flex-col  gap-3">
+      <p>{w("songAlreadyExist")}</p>
+      <div className="flex justify-end  gap-3">
+        <Button
+          onClick={() => {
+            closeModalBox(setIsSongExist, originParentTriggerRef);
+          }}
+        >
+          {w("cancel")}
+        </Button>
+        <Button onClick={handleAdd}>{w("add")}</Button>
+      </div>
     </div>
   );
 }
