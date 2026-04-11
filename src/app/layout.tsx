4f1a9e606d30d86272f38f64861352cbf797c9d5
aungchanpyae33 <khantlu24@gmail.com>
2026-04-11 19:33:39 +0630

    fix: add missing verification metadata for Google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ export const metadata: Metadata = {
     url: "/",
     siteName: "Bubble",
   },
+  verification: {
+    google: "qUiD6zMa2G5hXsaRx_P2lfhYardVPp1UbjZDHvj_ulA",
+  },
   icons: {
     icon: [
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
